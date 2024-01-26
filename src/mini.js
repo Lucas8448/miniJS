e87fd89303be3c76247a4773049cc6d0a13ffabe
Lucas Bateson <lucas.bateson@gmail.com>
2024-01-26 22:53:37 +0100

    Down to only 17 lines

--- src/mini.js
+++ src/mini.js
@@ -1,37 +1,17 @@
-
 const mini = (() => {
     const routes = {};
-    const renderComponent = (component) => {
-        const template = document.createElement('template');
-        template.innerHTML = component.render().trim();
-        return template.content.firstChild;
-    };
-
-    const render = (component, container) => {
-        const element = renderComponent(component);
+    const render = (component, container = document.querySelector('#app')) => {
         container.innerHTML = '';
-        container.appendChild(element);
-        if (component.afterRender) {
-            component.afterRender();
-        }
+        container.insertAdjacentHTML('afterbegin', component.render().trim());
+        component.afterRender?.();
     };
-
-    const route = (path, component) => {
-        routes[path] = component;
-    };
-
+    const route = (path, component) => routes[path] = component;
     const router = () => {
-        const path = location.hash.slice(1) || '/';
-        const component = routes[path];
-        if (component) {
-            render(component, document.querySelector('#app'));
-        }
+        const component = routes[location.hash.slice(1) || '/'];
+        if (component) render(component);
     };
-
     window.addEventListener('hashchange', router);
     window.addEventListener('load', router);
-
     return { render, route, router };
 })();
-
 export default mini;
