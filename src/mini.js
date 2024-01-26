2149db8e70f7615c7cb902a20ce5108afcaef0a2
Lucas Bateson <lucas.bateson@gmail.com>
2024-01-26 23:08:40 +0100

    Add vDOM to only rerender changed components

--- src/mini.js
+++ src/mini.js
@@ -1,17 +1,80 @@
 const mini = (() => {
     const routes = {};
-    const render = (component, container = document.querySelector('#app')) => {
-        container.innerHTML = '';
-        container.insertAdjacentHTML('afterbegin', component.render().trim());
+    let currentComponent = null;
+
+    const createVNode = (html) => {
+        const template = document.createElement('template');
+        template.innerHTML = html.trim();
+        return template.content.firstChild || document.createTextNode('');
+    };
+
+    const updateElement = (parent, newNode, oldNode, index = 0) => {
+        if (!oldNode && newNode) {
+            parent.appendChild(newNode);
+        } else if (oldNode && !newNode) {
+            if (parent.childNodes[index]) {
+                parent.removeChild(parent.childNodes[index]);
+            }
+        } else if (newNode && oldNode && changed(newNode, oldNode)) {
+            parent.replaceChild(newNode, parent.childNodes[index]);
+        } else if (newNode && oldNode && newNode.nodeType === Node.ELEMENT_NODE) {
+        updateAttributes(oldNode, newNode);
+        const maxLength = Math.max(newNode.childNodes.length, oldNode.childNodes.length);
+        for (let i = 0; i < maxLength; i++) {
+            updateElement(oldNode, newNode.childNodes[i], oldNode.childNodes[i], i);
+        }
+    }
+};
+
+
+    const updateAttributes = (oldNode, newNode) => {
+        const oldAttributes = oldNode.attributes;
+        const newAttributes = newNode.attributes;
+        for (let i = 0; i < newAttributes.length; i++) {
+            const attrName = newAttributes[i].name;
+            const attrValue = newAttributes[i].value;
+            if (oldNode.getAttribute(attrName) !== attrValue) {
+                oldNode.setAttribute(attrName, attrValue);
+            }
+        }
+        for (let i = 0; i < oldAttributes.length; i++) {
+            const attrName = oldAttributes[i].name;
+            if (!newNode.hasAttribute(attrName)) {
+                oldNode.removeAttribute(attrName);
+            }
+        }
+    };
+
+    const changed = (node1, node2) => {
+        return typeof node1 !== typeof node2 ||
+            node1.nodeType === Node.TEXT_NODE && node1.textContent !== node2.textContent ||
+            node1.nodeName !== node2.nodeName;
+    };
+
+    const renderComponent = (component) => {
+        const newVNode = createVNode(component.render());
+        const container = document.querySelector('#app');
+        const oldVNode = container.childNodes[0];
+        if (!currentComponent) {
+            container.appendChild(newVNode);
+        } else {
+            updateElement(container, newVNode, oldVNode);
+        }
+        currentComponent = component;
         component.afterRender?.();
     };
+
     const route = (path, component) => routes[path] = component;
+
     const router = () => {
         const component = routes[location.hash.slice(1) || '/'];
-        if (component) render(component);
+        if (component && component !== currentComponent) renderComponent(component);
     };
+
     window.addEventListener('hashchange', router);
     window.addEventListener('load', router);
-    return { render, route, router };
+
+    return { route, router };
 })();
+
 export default mini;
